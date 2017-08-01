ebb9f3bc4a9abed7dc5e41426153411eab3f0785
王晓东 <592901924@qq.com>
2017-08-01 23:14:41 +0800

    安装numpy

---
+++ 20170801/numpy.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python env
+
+__author__ = 'wangxiaodong'
+# project name:pythonstudy
+#   file name:numpy
+#   auther:wangxiaodong
+#   create date:2017/8/1 22:42
+#   change auther:
+#   change date:
+#   version:
+#   using enviroment:Django 1.13.0
+#   python version:python 3.5.2
+#   usage:
+
+import numpy
+
+numpy
