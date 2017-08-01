e9e644bce4ff68ba94cfec7b6bcd54a0b11877bf
wangyincheng <979411006@qq.com>
2017-08-01 21:53:32 +0800

    commitmessage

---
+++ 20170728/helloword.py
@@ -0,0 +1,13 @@
+#!/usr/env python
+
+#    name='',
+#    version='',
+#    packages=,
+#    author='',
+#    create time= 
+#    change author=  
+#    change time=  
+#    author_email='',
+#    msg=
+
+print('hello word!')
---
+++ 20170728/writefile.py
@@ -0,0 +1,16 @@
+#!/usr/env python
+#    name='',
+#    version='',
+#    packages=,
+#    author='',
+#    create time= 
+#    change author=  
+#    change time=  
+#    author_email='',
+#    msg=
+
+#with open('D:/pycharmworkspace/pythonstudy/20170728/HELLOWORD.TXT',mode='w',encoding='utf-8') as file:
+#    file.writelines('hello word!')
+
+with open('D:/test/HELLOWORD.TXT',mode='w',encoding='utf-8') as file:
+    file.writelines('hello word!')
